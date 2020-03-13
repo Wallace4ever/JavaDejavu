791c647fd8d97bf9b6051a65f589f0bdfcc9e4af
Wallace <xpkllp@hotmail.com>
2020-03-13 20:57:52 +0800

    Deleting unnessary exception handling.

--- src/edu/seu/Main.java
+++ src/edu/seu/Main.java
@@ -5,7 +5,7 @@ package edu.seu;
  */
 public class Main {
 
-    public static void main(String[] args) throws UserException {
+    public static void main(String[] args){
         String[]names={"\njava.version","os.arch","os.name","os.version","user.dir","user.home","user.name"};
         for (String name : names) {
             System.out.println(name + ": " + System.getProperty(name));
